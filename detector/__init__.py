b6d3e4472237075962ea1d119991931256afffe0
Lander-CS <aragaolanderson08@gmail.com>
2026-03-16 15:59:47 -0300

    Atualizer o domain indicators que tava me causando problema

--- detector/__init__.py
+++ detector/__init__.py
@@ -2,3 +2,4 @@ from .url_analyzer import analyze_url
 from .domain_analyzer import analyze_domain
 from .html_analyzer import analyze_html
 from .typosquat_detector import detect_typosquatting
+from .domain_analyzer import analyze_domain_indicators
